ce7ac49991a44f8d4a3e72888074ac9b7efa1087
fatima-zerrouk <fatima.zerrouk@laplateforme.io>
2026-06-30 13:33:47 +0200

    Suppression console log

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -4,8 +4,7 @@
 export function useFetch() {
   // const { logout } = useContext(AuthContext);
   const API_URL = import.meta.env.VITE_API_URL;
-  console.log("ENV =", import.meta.env);
-  console.log("API_URL =", import.meta.env.VITE_API_URL);
+
   
   async function apiFetch(url, options = {}) {
     const token = localStorage.getItem('token'); //récupère token
